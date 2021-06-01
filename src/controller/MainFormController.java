b0c5fc39dd290047c1b373428fdcc7a150a7a8e8
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-01 13:55:23 +0530

    Setup the basics of main form

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import com.jfoenix.controls.JFXRippler;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+
+    public JFXRippler rprAddNewStudent;
+    public AnchorPane pneAddNewStudent;
+    public JFXRippler rprSearchStudents;
+    public AnchorPane pneSearchStudents;
+
+    public void initialize(){
+        rprAddNewStudent.setControl(pneAddNewStudent);
+        rprSearchStudents.setControl(pneSearchStudents);
+
+
+    }
 }
