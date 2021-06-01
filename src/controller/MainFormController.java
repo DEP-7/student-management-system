c51420db5384e7a0c7604112c37ec6accb4e6b4f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-01 17:03:38 +0530

    Add StudentForm and SearchStudentForm with navigations

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,9 +1,17 @@
 package controller;
 
 import com.jfoenix.controls.JFXRippler;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
 
@@ -28,6 +36,41 @@ public class MainFormController {
     public void pneSearchStudents_OnKeyPress(KeyEvent keyEvent) {
         if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE){
             rprSearchStudents.createManualRipple().run();
+        }
+    }
+
+    public void pneSearchStudents_OnMouseClicked(MouseEvent mouseEvent) {
+        navigate("Search Students", "/view/SearchStudentsForm.fxml");
+    }
+
+    public void pneAddNewStudent_OnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        navigate("Add New Student", "/view/StudentForm.fxml");
+    }
+
+    private void navigate(String title, String url){
+        try {
+            Parent root = FXMLLoader.load(this.getClass().getResource(url));
+            Scene addNewStudentScene = new Scene(root);
+            Stage primaryStage = (Stage) (rprAddNewStudent.getScene().getWindow());
+            primaryStage.setResizable(true);
+            primaryStage.setScene(addNewStudentScene);
+            primaryStage.setTitle(title);
+            primaryStage.centerOnScreen();
+            Platform.runLater(() -> primaryStage.setResizable(false));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void pneAddNewStudent_OnKeyReleased(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE){
+            navigate("Add New Student", "/view/StudentForm.fxml");
+        }
+    }
+
+    public void pneSearchStudents_OnKeyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE){
+            navigate("Search Students", "/view/SearchStudentsForm.fxml");
         }
     }
 }
