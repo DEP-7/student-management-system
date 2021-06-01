ec98ef2325a44086537d9cc0f177479d1ba534f9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-01 16:35:21 +0530

    Add keyboard navigation to the main form

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import com.jfoenix.controls.JFXRippler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class MainFormController {
@@ -13,7 +15,19 @@ public class MainFormController {
     public void initialize(){
         rprAddNewStudent.setControl(pneAddNewStudent);
         rprSearchStudents.setControl(pneSearchStudents);
+        pneAddNewStudent.setFocusTraversable(true);
+        pneSearchStudents.setFocusTraversable(true);
+    }
 
+    public void pneAddNewStudent_OnKeyPress(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE){
+            rprAddNewStudent.createManualRipple().run();
+        }
+    }
 
+    public void pneSearchStudents_OnKeyPress(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE){
+            rprSearchStudents.createManualRipple().run();
+        }
     }
 }
