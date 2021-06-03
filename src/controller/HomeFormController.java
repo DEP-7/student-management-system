af7cd05c63d78a98b6f7316cc941483e2c42e4c1
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-03 22:03:57 +0530

    Move codes from MainFormController to HomeFormController

--- src/controller/HomeFormController.java
+++ src/controller/HomeFormController.java
@@ -1,4 +1,76 @@
 package controller;
 
+import com.jfoenix.controls.JFXRippler;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomeFormController {
+
+    public JFXRippler rprAddNewStudent;
+    public AnchorPane pneAddNewStudent;
+    public JFXRippler rprSearchStudents;
+    public AnchorPane pneSearchStudents;
+
+    public void initialize() {
+        rprAddNewStudent.setControl(pneAddNewStudent);
+        rprSearchStudents.setControl(pneSearchStudents);
+        pneAddNewStudent.setFocusTraversable(true);
+        pneSearchStudents.setFocusTraversable(true);
+    }
+
+    public void pneAddNewStudent_OnKeyPress(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
+            rprAddNewStudent.createManualRipple().run();
+        }
+    }
+
+    public void pneSearchStudents_OnKeyPress(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
+            rprSearchStudents.createManualRipple().run();
+        }
+    }
+
+    public void pneSearchStudents_OnMouseClicked(MouseEvent mouseEvent) {
+        navigate("Search Students", "/view/SearchStudentsForm.fxml");
+    }
+
+    public void pneAddNewStudent_OnMouseClicked(MouseEvent mouseEvent) throws IOException {
+        navigate("Add New Student", "/view/StudentForm.fxml");
+    }
+
+    private void navigate(String title, String url) {
+        try {
+            Parent root = FXMLLoader.load(this.getClass().getResource(url));
+            Scene addNewStudentScene = new Scene(root);
+            Stage primaryStage = (Stage) (rprAddNewStudent.getScene().getWindow());
+            primaryStage.setResizable(true);
+            primaryStage.setScene(addNewStudentScene);
+            primaryStage.setTitle(title);
+            primaryStage.centerOnScreen();
+            Platform.runLater(() -> primaryStage.setResizable(false));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pneAddNewStudent_OnKeyReleased(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
+            navigate("Add New Student", "/view/StudentForm.fxml");
+        }
+    }
+
+    public void pneSearchStudents_OnKeyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
+            navigate("Search Students", "/view/SearchStudentsForm.fxml");
+        }
+    }
 }
--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,29 +1,15 @@
 package controller;
 
-import com.jfoenix.controls.JFXRippler;
 import javafx.application.Platform;
-import javafx.collections.MapChangeListener;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
-import java.io.IOException;
-
 public class MainFormController {
 
-    public JFXRippler rprAddNewStudent;
-    public AnchorPane pneAddNewStudent;
-    public JFXRippler rprSearchStudents;
-    public AnchorPane pneSearchStudents;
     public ImageView imgClose;
     public ImageView imgMinimize;
     public AnchorPane pneAppBar;
@@ -32,18 +18,14 @@ public class MainFormController {
     private double yMousePos;
 
     public void initialize() {
-        rprAddNewStudent.setControl(pneAddNewStudent);
-        rprSearchStudents.setControl(pneSearchStudents);
-        pneAddNewStudent.setFocusTraversable(true);
-        pneSearchStudents.setFocusTraversable(true);
         initWindow();
     }
 
-    private void initWindow(){
+    private void initWindow() {
         lblTitle.setMouseTransparent(true);
 
-        Platform.runLater(()->{
-            lblTitle.setText(((Stage)(imgClose.getScene().getWindow())).getTitle());
+        Platform.runLater(() -> {
+            lblTitle.setText(((Stage) (imgClose.getScene().getWindow())).getTitle());
         });
 
         pneAppBar.setOnMousePressed(event -> {
@@ -66,52 +48,5 @@ public class MainFormController {
         imgMinimize.setOnMouseEntered(event -> imgMinimize.setImage(new Image("/view/assets/icons/minimize-hover.png")));
         imgMinimize.setOnMouseExited(event -> imgMinimize.setImage(new Image("/view/assets/icons/minimize.png")));
         imgMinimize.setOnMouseClicked(event -> ((Stage) (imgClose.getScene().getWindow())).setIconified(true));
-    }
-
-    public void pneAddNewStudent_OnKeyPress(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
-            rprAddNewStudent.createManualRipple().run();
-        }
-    }
-
-    public void pneSearchStudents_OnKeyPress(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
-            rprSearchStudents.createManualRipple().run();
-        }
-    }
-
-    public void pneSearchStudents_OnMouseClicked(MouseEvent mouseEvent) {
-        navigate("Search Students", "/view/SearchStudentsForm.fxml");
-    }
-
-    public void pneAddNewStudent_OnMouseClicked(MouseEvent mouseEvent) throws IOException {
-        navigate("Add New Student", "/view/StudentForm.fxml");
-    }
-
-    private void navigate(String title, String url) {
-        try {
-            Parent root = FXMLLoader.load(this.getClass().getResource(url));
-            Scene addNewStudentScene = new Scene(root);
-            Stage primaryStage = (Stage) (rprAddNewStudent.getScene().getWindow());
-            primaryStage.setResizable(true);
-            primaryStage.setScene(addNewStudentScene);
-            primaryStage.setTitle(title);
-            primaryStage.centerOnScreen();
-            Platform.runLater(() -> primaryStage.setResizable(false));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void pneAddNewStudent_OnKeyReleased(KeyEvent keyEvent) throws IOException {
-        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
-            navigate("Add New Student", "/view/StudentForm.fxml");
-        }
-    }
-
-    public void pneSearchStudents_OnKeyReleased(KeyEvent keyEvent) {
-        if (keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE) {
-            navigate("Search Students", "/view/SearchStudentsForm.fxml");
-        }
     }
 }
